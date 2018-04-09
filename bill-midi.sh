be339f2dfe8523a137c8a498363ca72557577cc8
Sergi Vilardell <sergivilardell13@gmail.com>
2018-04-09 20:40:30 +0200

    Add files via upload

--- bill-midi.sh
+++ bill-midi.sh
@@ -4,47 +4,19 @@
 #	Alfredo Hernández <aldomann.designs@gmail.com>
 #	Alejandro Jiménez <aljrico@gmail.com>
 
-OUTPUT=$HOME/"smf2txt-outputs"
+output_folder=$HOME/"Bach-midi2text"
 
-if [ ! -d $OUTPUT ]; then
-	mkdir $OUTPUT
-fi
-
-# For files in parent folder
-function parent {
-	for FILE in *.mid; do
-		NAME=${FILE##*/}
-		NAME=${NAME%.mid}
-		array=($NAME)
-		FNAME=${array[0]}
-		NAME=${NAME// /_}
-
-		OUTPUT=$HOME/"smf2txt-outputs"/$FNAME
-			if [ ! -d $OUTPUT ]; then
-				mkdir $OUTPUT
-			fi
-
-		smf2txt -f "%o #o %p %d #d %c %t" "$FILE" > "$OUTPUT"/"$NAME".txt
-	done
-}
-
-# For files in subfolders
-function children {
-	for FOLDER in */; do
-		for FILE in "$FOLDER"*.mid; do
-			FNAME=${FOLDER// /_}
-			FNAME=${FNAME%/}
-			NAME=${FILE##*/}
-			NAME=${NAME%.mid}
-			NAME=${NAME// /_}
-			OUTPUT=$HOME/"smf2txt-outputs"/$FNAME
-			if [ ! -d $OUTPUT ]; then
-				mkdir $OUTPUT
-			fi
-			smf2txt -f "%o #o %p %d #d %c %t" "$FILE" > "$OUTPUT"/"$FNAME"-"$NAME".txt
-		done
-	done
-}
-
-parent
-children
+shopt -s globstar
+for file in **/*.mid; do
+	echo $file
+	name=${file##*/}
+	name=${name%.mid}
+	name=${name// /_}
+	IFS='/' read -ra array <<< "$file"
+	fname=${array[0]}
+	output=$output_folder/$fname
+		if [ ! -d $output ]; then
+			mkdir $output
+		fi
+	smf2txt -f "%o #o %p %d #d %c %t" "$file" > "$output"/"$name".txt;
+done
